0bf5ea4c0b41fe6c7c7705d34ddbfe94ce86ae58
AcV134 <anirudh.c.vinayan@gmail.com>
2023-07-11 23:10:31 +0530

    updated project

--- Pharmacutical DBMS/src/App.java
+++ Pharmacutical DBMS/src/App.java
@@ -6,19 +6,15 @@ import javax.swing.JPanel;
 
 
 public class App{  
-    public static void main(String s[]) {  
-        JFrame frame = new JFrame("JFrame Example");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel("JFrame By Example");  
-        JButton button = new JButton();  
-        button.setText("Button");  
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(200, 300);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
-    }  
+
+    public static JFrame CreateFrame(String s){
+            JFrame frame = new JFrame(s); 
+            return frame;
+        }
+    
+     
+        
+    public static void main(String[] args){
+        JFrame f1 = CreateFrame("Pharmacy DBMS");
+    }
 }  
